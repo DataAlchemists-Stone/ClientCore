a25a57efee14dad05e2bb0715d714f36246a90dc
anabeggiato <ana.beggiato@sou.inteli.edu.br>
2024-08-25 15:56:28 -0300

    feat: adiciona a homepage

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def registro():
     return render_template('registro.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
