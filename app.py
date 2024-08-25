d19f96bac1733a809c9688a44dcb58e5de052aee
anabeggiato <ana.beggiato@sou.inteli.edu.br>
2024-08-25 02:00:21 -0300

    feat: adiciona tela de registro

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('login.html')
 
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
